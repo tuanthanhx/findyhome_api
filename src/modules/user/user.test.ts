b1a4c815d52727dcd30367f73b598b572ff22a0b
FASTCODING Thanh <ngo.thanh@fastcoding.jp>
2025-02-20 23:12:00 +0700

    [upd]

--- src/modules/user/user.test.ts
+++ src/modules/user/user.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import { faker } from '@faker-js/faker';
 import app from '../../app';
 import {
   connectTestDB,
@@ -17,6 +18,62 @@ afterAll(async () => {
 beforeEach(async () => {
   await User.deleteMany();
 });
+
+const userName01 = faker.person.fullName();
+const user01 = {
+  email: faker.internet.email({ firstName: userName01 }),
+  username: faker.internet.username({ firstName: userName01 }),
+  password: faker.internet.password(),
+  roles: [2],
+  status: faker.helpers.arrayElement([0, 1]),
+  avatar: faker.image.avatar(),
+  name: userName01,
+  dob: faker.date.birthdate({ min: 18, max: 60, mode: 'age' }),
+  nationalId: faker.string.numeric(12),
+  phone: `+84${faker.string.numeric(8)}`,
+  address: faker.location.streetAddress(),
+  socialProfile: {
+    facebook: faker.internet.url(),
+    tiktok: faker.internet.url(),
+  },
+  baseSalary: faker.number.int({ min: 500, max: 5000 }),
+  bankAccount: {
+    bankName: faker.company.name(),
+    branch: faker.location.city(),
+    accountNumber: faker.string.numeric(10),
+  },
+  bio: faker.person.bio(),
+};
+
+const userName02 = faker.person.fullName();
+const user02 = {
+  email: faker.internet.email({ firstName: userName02 }),
+  username: faker.internet.username({ firstName: userName02 }),
+  password: faker.internet.password(),
+  roles: [6],
+  status: faker.helpers.arrayElement([0, 1]),
+  avatar: faker.image.avatar(),
+  name: userName02,
+  dob: faker.date.birthdate({ min: 18, max: 60, mode: 'age' }),
+  nationalId: faker.string.numeric(12),
+  phone: `+84${faker.string.numeric(8)}`,
+  address: faker.location.streetAddress(),
+  socialProfile: {
+    facebook: faker.internet.url(),
+    tiktok: faker.internet.url(),
+  },
+  baseSalary: faker.number.int({ min: 500, max: 5000 }),
+  bankAccount: {
+    bankName: faker.company.name(),
+    branch: faker.location.city(),
+    accountNumber: faker.string.numeric(10),
+  },
+  bio: faker.person.bio(),
+};
+
+const insertUser = async (user: Record<string, any>) => {
+  await User.create(user);
+};
 
 describe('User API', () => {
   describe('POST /users', () => {
@@ -65,21 +122,14 @@ describe('User API', () => {
     });
   });
   describe('GET /users', () => {
-    it('should get list of users', async () => {
+    it('should get a list of users', async () => {
+      await insertUser(user01);
+      await insertUser(user02);
+
       const response = await request(app).get('/api/v1/users');
 
       expect(response.status).toBe(200);
-      // expect(response.body.user.name).toBe(userData.name);
-      // expect(response.body.user.email).toBe(userData.email);
-      // expect(response.body.user.address).toBe(userData.address);
+      // console.log(JSON.stringify(response.body, null, 2));
     });
-
-    // it('should not create a user with invalid data', async () => {
-    //   const response = await request(app).post('/api/v1/users').send({
-    //     foo: 'bar',
-    //   });
-
-    //   expect(response.status).toBe(400);
-    // });
   });
 });
